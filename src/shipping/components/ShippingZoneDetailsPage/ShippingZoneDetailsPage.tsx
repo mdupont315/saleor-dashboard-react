weestack/saleor-dashboard

Add warehouse select

src/shipping/components/ShippingZoneDetailsPage/ShippingZoneDetailsPage.tsx
@@ -11,6 +11,7 @@ import Grid from "@saleor/components/Grid";
 import PageHeader from "@saleor/components/PageHeader";
 import SaveButtonBar from "@saleor/components/SaveButtonBar";
 import createSingleAutocompleteSelectHandler from "@saleor/utils/handlers/singleAutocompleteSelectChangeHandler";
+import useStateFromProps from "@saleor/hooks/useStateFromProps";
 import { maybe } from "../../../misc";
 import { UserError } from "../../../types";
 import { ShippingMethodTypeEnum } from "../../../types/globalTypes";
@@ -68,7 +69,7 @@ const ShippingZoneDetailsPage: React.FC<ShippingZoneDetailsPageProps> = ({
     name: maybe(() => shippingZone.name, ""),
     warehouse: maybe(() => shippingZone.warehouses[0].id, "")
   };
-  const [warehouseDisplayValue, setWarehouseDisplayValue] = React.useState(
+  const [warehouseDisplayValue, setWarehouseDisplayValue] = useStateFromProps(
     maybe(() => shippingZone.warehouses[0].name, "")
   );
 

mdupont315/saleor-dashboard-react

src/shipping/components/ShippingZoneDetailsPage/ShippingZoneDetailsPage.tsx
@@ -13,6 +13,7 @@ import SaveButtonBar from "@saleor/components/SaveButtonBar";
 import { ShippingErrorFragment } from "@saleor/shipping/types/ShippingErrorFragment";
 import createMultiAutocompleteSelectHandler from "@saleor/utils/handlers/multiAutocompleteSelectChangeHandler";
 import { MultiAutocompleteChoiceType } from "@saleor/components/MultiAutocompleteSelectField";
+import useStateFromProps from "@saleor/hooks/useStateFromProps";
 import { maybe } from "../../../misc";
 import { ShippingMethodTypeEnum } from "../../../types/globalTypes";
 import {
@@ -78,7 +79,7 @@ const ShippingZoneDetailsPage: React.FC<ShippingZoneDetailsPageProps> = ({
     name: shippingZone?.name || "",
     warehouses: shippingZone?.warehouses.map(warehouse => warehouse.id) || []
   };
-  const [warehouseDisplayValues, setWarehouseDisplayValues] = React.useState(
+  const [warehouseDisplayValues, setWarehouseDisplayValues] = useStateFromProps(
     shippingZone?.warehouses.map(warehouseToChoice)
   );
 